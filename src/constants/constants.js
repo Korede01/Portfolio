77aadef97804a956f847d50b93da6a8fc8e629a3
Korede <erinlekorede@gmail.com>
2023-03-14 09:42:48 +0000

    Main

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Face Recognition Model',
+    description: "Using Python, TensorFlow and Keras, I developed a machine learning model to detect the face of famous celebrities. The model was trained using a dataset of popular celebrities to detect faces of celebrities with an accuracy of 96%. Not just celebrities, but any image given to it",
+      image: '/images/17.jfif',
+      tags: ['Python', 'TensorFlow', 'Keras'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Brain Tumor Detection',
+    description:"Built a brain tumor detection system using TensorFlow, Python, Keras and Scikit-Learn and deployed it using Docker and AWS. The model possesses an accuracy of 97%. Its main purpose is to detect if a patient has a brain tumor/cancer from the MRI given into the system.",
+    image: '/images/18.jfif',
+    tags: ['Python', 'TensorFlow', 'Keras','Docker'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Hand Pose Estimation for VR Applications',
+    description: "Developed a deep learning model for hand pose estimation using a custom dataset and various 3D vision algorithms, including geometric feature matching and model-based posed estimation. Integrated the model with a VR headset to provide real-time hand tracking for immersive experiences.",
+      image: '/images/19.jfif',
+      tags: ['Python', 'TensorFlow', 'OpenCV'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Object Detection For Autonomous Drones',
+    description: "Created a computer vision system for object detection and tracking on a drone platform using traditional 3D vision algorithms and SLAM tracking. ",
+    image: '/images/20.jfif',
+    tags: ['Python', 'TensorFlow', 'Keras', 'SLAM'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: 'Started my journey', },
+  { year: 2020, text: 'Earned my certificates', },
+  { year: 2021, text: 'Worked on developing projects', },
+  { year: 2022, text: 'Working as a freelance engineer', },
+  { year: 2023, text: 'Working as a freelance engineer', },
+];
