867e2225c593af89e8d06fc32612212d5e55e752
Erinle Oluwakorede <erinlekorede@gmail.com>
2023-04-23 14:56:30 +0100

    main

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -39,8 +39,8 @@ export const projects = [
 
 export const TimeLineData = [
   { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: 'Earned my certificates', },
-  { year: 2021, text: 'Worked on developing projects', },
-  { year: 2022, text: 'Working as a freelance engineer', },
-  { year: 2023, text: 'Working as a freelance engineer', },
+  { year: 2020, text: 'Machine Learning Engineer Intern', },
+  { year: 2021, text: 'Junior, Machine Learning Engineer', },
+  { year: 2022, text: 'Mid-Senior, Machine Learning Engineer', },
+  { year: 2023, text: 'Mid-Senior, Machine Learning Engineer', },
 ];
